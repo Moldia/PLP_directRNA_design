5655c25dc0a38d8395cbd7b409e594e01113a48c
Marco Grillo <marcogrillo80@gmail.com>
2023-02-13 12:49:34 +0100

    Update __init__.py
    Updated the init file because it was failing to import correctly the package

--- __init__.py
+++ __init__.py
@@ -1,4 +1,1 @@
-from PLP_directRNA_design.PLP_directRNA_design.PLP_design import (extractseq,extract_seqs,findtargets,plot_alignment,
-import PLP_directRNA_design
-
 from PLP_directRNA_design.PLP_design import *
